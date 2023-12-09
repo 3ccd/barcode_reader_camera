4558ac777ac544471c190eea236f773fd15b6906
UraChihiro <chiichii19991104@live.com>
2023-12-09 14:41:57 +0900

    add version-2 camera support

--- scan_cam_v2.py
+++ scan_cam_v2.py
@@ -7,12 +7,20 @@ pixels = 3694
 
 com = None
 
+offset = 70
+# ガンマ変換用の数値準備
+gamma = 3  # γ値を指定
+img2gamma = np.zeros((256, 1), dtype=np.uint8)  # ガンマ変換初期値
+
+# 公式適用
+for i in range(256):
+    img2gamma[i][0] = 255 * (float(i) / 255) ** (1.0 / gamma)
+
 try:
     com = serial.Serial("COM9", baudrate=115200, parity=serial.PARITY_NONE)
 except serial.SerialException as e:
     print(e)
     exit(-1)
-
 
 if com.is_open:
     com.close()
@@ -23,7 +31,6 @@ except serial.SerialException as e:
     print(e)
     exit(-1)
 
-
 frame = np.zeros((pixels, 6000), dtype=np.uint8)
 
 com.flush()
@@ -33,11 +40,15 @@ print("start receiving")
 while True:
     recv = com.read_until(b'\xff\xff\xff\xff')
     if len(recv) != pixels + 4:
-        print("invalid data")
+        print("invalid data length : {}".format(len(recv)))
         continue
 
     line = np.frombuffer(recv[:-4], dtype=np.uint8)
     line = 255 - line
+
+    line[line <= offset] = 0
+    line[line > offset] = line[line > offset] - offset
+    line = line * (255 / (255 - offset))
 
     # line[line < bl] = 0
     # line = (line - bl) * v
@@ -46,7 +57,8 @@ while True:
     frame = np.roll(frame, (0, 1), axis=(0, 1))
     frame[:, 0] = line
 
-    prev_img = cv2.resize(frame, None, fx=0.2, fy=0.2)
+    prev_img = cv2.LUT(frame, img2gamma)
+    prev_img = cv2.resize(prev_img, None, fx=0.2, fy=0.2)
 
     cv2.imshow("preview", prev_img)
 
@@ -54,4 +66,6 @@ while True:
     if ret == ord("q"):
         break
 
+frame = cv2.LUT(frame, img2gamma)
+cv2.imwrite("out.jpg", frame)
 com.close()
