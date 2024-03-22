5267415adb23b9bf3b1eb205fb5d0b3e92177b4b
UraChihiro <chiichii19991104@live.com>
2024-03-23 02:10:35 +0900

    add camera control function

--- scan_cam_v2.py
+++ scan_cam_v2.py
@@ -1,14 +1,18 @@
 import time
+import sys
 
 import serial
 
 import numpy as np
 import cv2
 
+args = sys.argv
+
 pixels = 3694
 exp = 20
 
 com = None
+preparing = False
 
 offset = 30
 # ガンマ変換用の数値準備
@@ -20,7 +24,7 @@ for i in range(256):
     img2gamma[i][0] = 255 * (float(i) / 255) ** (1.0 / gamma)
 
 try:
-    com = serial.Serial("COM9", baudrate=115200, parity=serial.PARITY_NONE, timeout=1)
+    com = serial.Serial(args[1], baudrate=115200, parity=serial.PARITY_NONE, timeout=1)
 except serial.SerialException as e:
     print(e)
     exit(-1)
@@ -45,24 +49,28 @@ com.write(b'\x03\x00')
 while True:
     recv = com.read_until(b'\xff\xff\xff\xff')
     if len(recv) != pixels + 4:
-        if len(recv) == 0:
-            break
         print("invalid data length : {}".format(len(recv)))
         continue
 
-    line = np.frombuffer(recv[:-4], dtype=np.uint8)
-    line = 255 - line
+    if len(recv) != 0:
+        preparing = False
 
-    line[line <= offset] = 0
-    line[line > offset] = line[line > offset] - offset
-    line = line * (255 / (255 - offset))
+        line = np.frombuffer(recv[:-4], dtype=np.uint8)
+        line = 255 - line
 
-    # line[line < bl] = 0
-    # line = (line - bl) * v
-    # line = 255 * np.power(line / 255, 1 / gamma)
+        line[line <= offset] = 0
+        line[line > offset] = line[line > offset] - offset
+        line = line * (255 / (255 - offset))
 
-    frame = np.roll(frame, (0, 1), axis=(0, 1))
-    frame[:, 0] = line
+        # line[line < bl] = 0
+        # line = (line - bl) * v
+        # line = 255 * np.power(line / 255, 1 / gamma)
+
+        frame = np.roll(frame, (0, 1), axis=(0, 1))
+        frame[:, 0] = line
+    else:
+        preparing = True
+        time.sleep(1)
 
     # prev_img = cv2.LUT(frame, img2gamma)
     prev_img = cv2.resize(frame, None, fx=0.2, fy=0.2)
@@ -72,6 +80,9 @@ while True:
 
     color = cv2.cvtColor(prev_img, cv2.COLOR_GRAY2BGR)
     cv2.putText(color, "Exposure : " + str(exp) + "ms", (0, 50), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3, cv2.LINE_AA)
+    if preparing:
+        cv2.putText(color, "Preparing...", (0, 100), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3,
+                    cv2.LINE_AA)
 
     cv2.imshow("preview", color)
     cv2.imshow("zoom", zoom_img)
